46f3f90415cb7a18f40c6cff248a2747fc3b193c
Euler <eulergomesrocha@gmail.com>
2025-06-11 10:57:15 -0300

    details fragment xml

---
+++ app/src/main/java/com/eulergomees/mybooks/DetailsFragment.kt
@@ -0,0 +1,40 @@
+package com.eulergomees.mybooks
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eulergomees.mybooks.databinding.FragmentDetailsBinding
+import com.eulergomees.mybooks.databinding.FragmentHomeBinding
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val viewModel: DetailsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/eulergomees/mybooks/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.eulergomees.mybooks
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
